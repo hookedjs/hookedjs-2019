55cccf6582c96449180c772ad1dfd8d7692a44e9
Brian Dombrowski <bdombro@gmail.com>
2019-02-25 17:51:02 -0500

    Form upgrades

---
+++ client-web/src/core/hooks/UseComponentSize.ts
@@ -0,0 +1,35 @@
+/**
+ * Allows components to switch on component size
+ *
+ * You may get the ref of a component using the following code:
+ * const ref = React.useRef(null);
+ *
+ */
+
+import * as React from "react";
+import { Debounce } from "~/core/utils/Debounce";
+
+export const UseComponentSize = (ref: React.MutableRefObject<null>): {width: number, height: number} => {
+  // @ts-ignore: ignore missing html prop
+  const getCurrentSize = () => ref && ref.current ? { width: ref.current.clientWidth, height: ref.current.clientHeight } : { width: 0, height: 0 };
+
+  const [size, setSize] = React.useState(getCurrentSize());
+
+  const handleResize = () => {
+    setSize(getCurrentSize());
+  };
+  const handleResizeDebounced = Debounce(handleResize, 20, true);
+
+  React.useLayoutEffect(
+    () => {
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResizeDebounced);
+      };
+    },
+    [ref.current]
+  ); // watching false means this only fires on mount/dismount
+
+  return size;
+};
---
+++ client-web/src/core/utils/Validators.ts
@@ -0,0 +1,22 @@
+export const DigitsRe = new RegExp(`^[0-9]+$`);
+
+export const AlphabeticRe = new RegExp(`^[a-zA-Z]+$`);
+
+export const AlphaNumericRe = new RegExp(`^[a-zA-Z0-9]+$`);
+
+export const EmailAddressRe = new RegExp(`^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$`);
+
+export const USPhoneNumbersRe = new RegExp(`^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$`);
+
+export const UsZipcodeRe = new RegExp(`^[0-9]{5}(?:-[0-9]{4})?$`);
+
+export const SlugRe = new RegExp(`^[a-z0-9-]+$`);
+
+// export const UrlRe = new RegExp(`((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)`);
+
+export const IpAddressRe = new RegExp(`^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`);
+
+export const HtmlTagRe = new RegExp(`^<([a-z]+)([^<]+)*(?:>(.*)</1>|s+/>)$`);
+
+// The password must export contain one lowercase letter, one uppercase letter, one number, one unique character such as !@#$%^&? and be at least 8 characters long.
+export const PasswordRe = new RegExp(`^.*(?=.{8,})(?=.*d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$`);
