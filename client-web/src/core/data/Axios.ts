23db1b491404ce5f639235980103e2665286c199
Brian Dombrowski <bdombro@gmail.com>
2019-04-11 17:04:29 -0400

    Upped the axios timeout

--- client-web/src/core/data/Axios.ts
+++ client-web/src/core/data/Axios.ts
@@ -4,5 +4,7 @@ import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: "/api/1.0",
-  timeout: 1000,
+  timeout: 10000,
 });
+
+// TODO: Handle timeouts gracefully with an alert or warning of some kind
--- client-web/src/core/data/AxiosCached.ts
+++ client-web/src/core/data/AxiosCached.ts
@@ -6,7 +6,7 @@ import memoryDriver from "localforage-memoryStorageDriver";
 // Create `axios` instance with pre-configured `axios-cache-adapter` attached to it
 export const AxiosCached = setup({
   baseURL: "/api/1.0",
-  timeout: 1000,
+  timeout: 10000,
 
   // `axios-cache-adapter` options
   cache: {
